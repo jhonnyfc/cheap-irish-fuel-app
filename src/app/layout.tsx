5a537c19ad499acf5894c176fb51a137880fb022
jhonnyfc <jhonnyfc88@gmail.com>
2025-02-09 23:02:36 +0000

    feat: add index pages

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -30,6 +30,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="canonical" href="https://cheapirishfuel.info/" />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
